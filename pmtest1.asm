51f2549c6834684189ced7890135009057d4c429
Crazing <1115261726@qq.com>
2018-03-30 20:31:02 +0800

    【修改时间：20180330  20:29】 【修改内容： 1、实模式下通过中断获取内存信息 2、保护模式下打印出内存信息 3、根据最大内存来初始化页表

--- pmtest1.asm
+++ pmtest1.asm
@@ -1,6 +1,6 @@
 ; ==========================================
-; pmtest6.asm
-; 编译方法：nasm pmtest6.asm -o pmtest6.com
+; pmtest7.asm
+; 编译方法：nasm pmtest7.asm -o pmtest7.com
 ; ==========================================
 
 %include	"pm.inc"	; 常量, 宏, 以及一些说明
@@ -14,10 +14,10 @@ org	0100h
 [SECTION .gdt]
 ; GDT
 ;                                         段基址,       段界限     , 属性
-LABEL_GDT:		Descriptor	       0,                 0, 0     		; 空描述符
+LABEL_GDT:		Descriptor	       0,                 0, 0			; 空描述符
 LABEL_DESC_NORMAL:	Descriptor	       0,            0ffffh, DA_DRW		; Normal 描述符
-LABEL_DESC_PAGE_DIR:	Descriptor   PageDirBase,              4095, DA_DRW				; Page Directory, 4K
-LABEL_DESC_PAGE_TBL:	Descriptor   PageTblBase,              1023, DA_DRW  | DA_LIMIT_4K		; Page Tables, 4M
+LABEL_DESC_PAGE_DIR:	Descriptor   PageDirBase,              4095, DA_DRW		; Page Directory
+LABEL_DESC_PAGE_TBL:	Descriptor   PageTblBase,      4096 * 8 - 1, DA_DRW		; Page Tables
 LABEL_DESC_CODE32:	Descriptor	       0,  SegCode32Len - 1, DA_C + DA_32	; 非一致代码段, 32
 LABEL_DESC_CODE16:	Descriptor	       0,            0ffffh, DA_C		; 非一致代码段, 16
 LABEL_DESC_DATA:	Descriptor	       0,	DataLen - 1, DA_DRW		; Data
@@ -44,10 +44,42 @@ SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT
 ALIGN	32
 [BITS	32]
 LABEL_DATA:
-SPValueInRealMode	dw	0
+; 实模式下使用这些符号
 ; 字符串
-PMMessage:		db	"In Protect Mode now. ^-^", 0	; 进入保护模式后显示此字符串
-OffsetPMMessage		equ	PMMessage - $$
+_szPMMessage:			db	"In Protect Mode now. ^-^", 0Ah, 0Ah, 0	; 进入保护模式后显示此字符串
+_szMemChkTitle:			db	"BaseAddrL BaseAddrH LengthLow LengthHigh   Type", 0Ah, 0	; 进入保护模式后显示此字符串
+_szRAMSize			db	"RAM size:", 0
+_szReturn			db	0Ah, 0
+; 变量
+_wSPValueInRealMode		dw	0
+_dwMCRNumber:			dd	0	; Memory Check Result
+_dwDispPos:			dd	(80 * 6 + 0) * 2	; 屏幕第 6 行, 第 0 列。
+_dwMemSize:			dd	0
+_ARDStruct:			; Address Range Descriptor Structure
+	_dwBaseAddrLow:		dd	0
+	_dwBaseAddrHigh:	dd	0
+	_dwLengthLow:		dd	0
+	_dwLengthHigh:		dd	0
+	_dwType:		dd	0
+
+_MemChkBuf:	times	256	db	0
+
+; 保护模式下使用这些符号
+szPMMessage		equ	_szPMMessage	- $$
+szMemChkTitle		equ	_szMemChkTitle	- $$
+szRAMSize		equ	_szRAMSize	- $$
+szReturn		equ	_szReturn	- $$
+dwDispPos		equ	_dwDispPos	- $$
+dwMemSize		equ	_dwMemSize	- $$
+dwMCRNumber		equ	_dwMCRNumber	- $$
+ARDStruct		equ	_ARDStruct	- $$
+	dwBaseAddrLow	equ	_dwBaseAddrLow	- $$
+	dwBaseAddrHigh	equ	_dwBaseAddrHigh	- $$
+	dwLengthLow	equ	_dwLengthLow	- $$
+	dwLengthHigh	equ	_dwLengthHigh	- $$
+	dwType		equ	_dwType		- $$
+MemChkBuf		equ	_MemChkBuf	- $$
+
 DataLen			equ	$ - LABEL_DATA
 ; END of [SECTION .data1]
 
@@ -74,7 +106,25 @@ LABEL_BEGIN:
 	mov	sp, 0100h
 
 	mov	[LABEL_GO_BACK_TO_REAL+3], ax
-	mov	[SPValueInRealMode], sp
+	mov	[_wSPValueInRealMode], sp
+
+	; 得到内存数
+	mov	ebx, 0
+	mov	di, _MemChkBuf
+.loop:
+	mov	eax, 0E820h
+	mov	ecx, 20
+	mov	edx, 0534D4150h
+	int	15h
+	jc	LABEL_MEM_CHK_FAIL
+	add	di, 20
+	inc	dword [_dwMCRNumber]
+	cmp	ebx, 0
+	jne	.loop
+	jmp	LABEL_MEM_CHK_OK
+LABEL_MEM_CHK_FAIL:
+	mov	dword [_dwMCRNumber], 0
+LABEL_MEM_CHK_OK:
 
 	; 初始化 16 位代码段描述符
 	mov	ax, cs
@@ -150,7 +200,7 @@ LABEL_REAL_ENTRY:		; 从保护模式跳回到实模式就到了这里
 	mov	es, ax
 	mov	ss, ax
 
-	mov	sp, [SPValueInRealMode]
+	mov	sp, [_wSPValueInRealMode]
 
 	in	al, 92h		; ┓
 	and	al, 11111101b	; ┣ 关闭 A20 地址线
@@ -167,10 +217,10 @@ LABEL_REAL_ENTRY:		; 从保护模式跳回到实模式就到了这里
 [BITS	32]
 
 LABEL_SEG_CODE32:
-	call	SetupPaging
-
 	mov	ax, SelectorData
 	mov	ds, ax			; 数据段选择子
+	mov	ax, SelectorData
+	mov	es, ax
 	mov	ax, SelectorVideo
 	mov	gs, ax			; 视频段选择子
 
@@ -181,32 +231,40 @@ LABEL_SEG_CODE32:
 
 
 	; 下面显示一个字符串
-	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
-	xor	esi, esi
-	xor	edi, edi
-	mov	esi, OffsetPMMessage	; 源数据偏移
-	mov	edi, (80 * 10 + 0) * 2	; 目的数据偏移。屏幕第 10 行, 第 0 列。
-	cld
-.1:
-	lodsb
-	test	al, al
-	jz	.2
-	mov	[gs:edi], ax
-	add	edi, 2
-	jmp	.1
-.2:	; 显示完毕
+	push	szPMMessage
+	call	DispStr
+	add	esp, 4
+
+	push	szMemChkTitle
+	call	DispStr
+	add	esp, 4
+
+	call	DispMemSize		; 显示内存信息
+
+	call	SetupPaging		; 启动分页机制
 
 	; 到此停止
 	jmp	SelectorCode16:0
 
 ; 启动分页机制 --------------------------------------------------------------
 SetupPaging:
-	; 为简化处理, 所有线性地址对应相等的物理地址.
+	; 根据内存大小计算应初始化多少PDE以及多少页表
+	xor	edx, edx
+	mov	eax, [dwMemSize]
+	mov	ebx, 400000h	; 400000h = 4M = 4096 * 1024, 一个页表对应的内存大小
+	div	ebx
+	mov	ecx, eax	; 此时 ecx 为页表的个数，也即 PDE 应该的个数
+	test	edx, edx
+	jz	.no_remainder
+	inc	ecx		; 如果余数不为 0 就需增加一个页表
+.no_remainder:
+	push	ecx		; 暂存页表个数
+
+	; 为简化处理, 所有线性地址对应相等的物理地址. 并且不考虑内存空洞.
 
 	; 首先初始化页目录
 	mov	ax, SelectorPageDir	; 此段首地址为 PageDirBase
 	mov	es, ax
-	mov	ecx, 1024		; 共 1K 个表项
 	xor	edi, edi
 	xor	eax, eax
 	mov	eax, PageTblBase | PG_P  | PG_USU | PG_RWW
@@ -215,10 +273,13 @@ SetupPaging:
 	add	eax, 4096		; 为了简化, 所有页表在内存中是连续的.
 	loop	.1
 
-	; 再初始化所有页表 (1K 个, 4M 内存空间)
+	; 再初始化所有页表
 	mov	ax, SelectorPageTbl	; 此段首地址为 PageTblBase
 	mov	es, ax
-	mov	ecx, 1024 * 1024	; 共 1M 个页表项, 也即有 1M 个页
+	pop	eax			; 页表个数
+	mov	ebx, 1024		; 每个页表 1024 个 PTE
+	mul	ebx
+	mov	ecx, eax		; PTE个数 = 页表个数 * 1024
 	xor	edi, edi
 	xor	eax, eax
 	mov	eax, PG_P  | PG_USU | PG_RWW
@@ -238,6 +299,54 @@ SetupPaging:
 
 	ret
 ; 分页机制启动完毕 ----------------------------------------------------------
+
+
+
+DispMemSize:
+	push	esi
+	push	edi
+	push	ecx
+
+	mov	esi, MemChkBuf
+	mov	ecx, [dwMCRNumber]	;for(int i=0;i<[MCRNumber];i++) // 每次得到一个ARDS(Address Range Descriptor Structure)结构
+.loop:					;{
+	mov	edx, 5			;	for(int j=0;j<5;j++)	// 每次得到一个ARDS中的成员，共5个成员
+	mov	edi, ARDStruct		;	{			// 依次显示：BaseAddrLow，BaseAddrHigh，LengthLow，LengthHigh，Type
+.1:					;
+	push	dword [esi]		;
+	call	DispInt			;		DispInt(MemChkBuf[j*4]); // 显示一个成员
+	pop	eax			;
+	stosd				;		ARDStruct[j*4] = MemChkBuf[j*4];
+	add	esi, 4			;
+	dec	edx			;
+	cmp	edx, 0			;
+	jnz	.1			;	}
+	call	DispReturn		;	printf("\n");
+	cmp	dword [dwType], 1	;	if(Type == AddressRangeMemory) // AddressRangeMemory : 1, AddressRangeReserved : 2
+	jne	.2			;	{
+	mov	eax, [dwBaseAddrLow]	;
+	add	eax, [dwLengthLow]	;
+	cmp	eax, [dwMemSize]	;		if(BaseAddrLow + LengthLow > MemSize)
+	jb	.2			;
+	mov	[dwMemSize], eax	;			MemSize = BaseAddrLow + LengthLow;
+.2:					;	}
+	loop	.loop			;}
+					;
+	call	DispReturn		;printf("\n");
+	push	szRAMSize		;
+	call	DispStr			;printf("RAM size:");
+	add	esp, 4			;
+					;
+	push	dword [dwMemSize]	;
+	call	DispInt			;DispInt(MemSize);
+	add	esp, 4			;
+
+	pop	ecx
+	pop	edi
+	pop	esi
+	ret
+
+%include	"lib.inc"	; 库函数
 
 SegCode32Len	equ	$ - LABEL_SEG_CODE32
 ; END of [SECTION .s32]
