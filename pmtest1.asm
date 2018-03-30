17dcbff73948846b10518c72368e2cd379194c04
Crazing <1115261726@qq.com>
2018-03-30 10:26:27 +0800

    【修改时间：20180330  10:25】 【修改内容：1、测试调用门代码（无特权级变化）

--- pmtest1.asm
+++ pmtest1.asm
@@ -1,6 +1,6 @@
 ; ==========================================
-; pmtest3.asm
-; 编译方法：nasm pmtest3.asm -o pmtest3.com
+; pmtest4.asm
+; 编译方法：nasm pmtest4.asm -o pmtest4.com
 ; ==========================================
 
 %include	"pm.inc"	; 常量, 宏, 以及一些说明
@@ -15,10 +15,14 @@ LABEL_GDT:		Descriptor	       0,                 0, 0     		; 空描述符
 LABEL_DESC_NORMAL:	Descriptor	       0,            0ffffh, DA_DRW		; Normal 描述符
 LABEL_DESC_CODE32:	Descriptor	       0,  SegCode32Len - 1, DA_C + DA_32	; 非一致代码段, 32
 LABEL_DESC_CODE16:	Descriptor	       0,            0ffffh, DA_C		; 非一致代码段, 16
+LABEL_DESC_CODE_DEST:	Descriptor	       0,SegCodeDestLen - 1, DA_C + DA_32	; 非一致代码段, 32
 LABEL_DESC_DATA:	Descriptor	       0,	DataLen - 1, DA_DRW		; Data
 LABEL_DESC_STACK:	Descriptor	       0,        TopOfStack, DA_DRWA + DA_32	; Stack, 32 位
 LABEL_DESC_LDT:		Descriptor	       0,        LDTLen - 1, DA_LDT		; LDT
 LABEL_DESC_VIDEO:	Descriptor	 0B8000h,            0ffffh, DA_DRW		; 显存首地址
+
+; 门                                            目标选择子,       偏移, DCount, 属性
+LABEL_CALL_GATE_TEST:	Gate		  SelectorCodeDest,          0,      0, DA_386CGate + DA_DPL0
 ; GDT 结束
 
 GdtLen		equ	$ - LABEL_GDT	; GDT长度
@@ -29,10 +33,13 @@ GdtPtr		dw	GdtLen - 1	; GDT界限
 SelectorNormal		equ	LABEL_DESC_NORMAL	- LABEL_GDT
 SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT
 SelectorCode16		equ	LABEL_DESC_CODE16	- LABEL_GDT
+SelectorCodeDest	equ	LABEL_DESC_CODE_DEST	- LABEL_GDT
 SelectorData		equ	LABEL_DESC_DATA		- LABEL_GDT
 SelectorStack		equ	LABEL_DESC_STACK	- LABEL_GDT
 SelectorLDT		equ	LABEL_DESC_LDT		- LABEL_GDT
 SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT
+
+SelectorCallGateTest	equ	LABEL_CALL_GATE_TEST	- LABEL_GDT
 ; END of [SECTION .gdt]
 
 [SECTION .data1]	 ; 数据段
@@ -92,6 +99,16 @@ LABEL_BEGIN:
 	shr	eax, 16
 	mov	byte [LABEL_DESC_CODE32 + 4], al
 	mov	byte [LABEL_DESC_CODE32 + 7], ah
+
+	; 初始化测试调用门的代码段描述符
+	xor	eax, eax
+	mov	ax, cs
+	shl	eax, 4
+	add	eax, LABEL_SEG_CODE_DEST
+	mov	word [LABEL_DESC_CODE_DEST + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_CODE_DEST + 4], al
+	mov	byte [LABEL_DESC_CODE_DEST + 7], ah
 
 	; 初始化数据段描述符
 	xor	eax, eax
@@ -213,11 +230,14 @@ LABEL_SEG_CODE32:
 
 	call	DispReturn
 
+	call	SelectorCallGateTest:0	; 测试调用门（无特权级变换），将打印字母 'C'。
+	;call	SelectorCodeDest:0
+
 	; Load LDT
 	mov	ax, SelectorLDT
 	lldt	ax
 
-	jmp	SelectorLDTCodeA:0	; 跳入局部任务
+	jmp	SelectorLDTCodeA:0	; 跳入局部任务，将打印字母 'L'。
 
 ; ------------------------------------------------------------------------
 DispReturn:
@@ -239,6 +259,25 @@ DispReturn:
 
 SegCode32Len	equ	$ - LABEL_SEG_CODE32
 ; END of [SECTION .s32]
+
+
+[SECTION .sdest]; 调用门目标段
+[BITS	32]
+
+LABEL_SEG_CODE_DEST:
+	;jmp	$
+	mov	ax, SelectorVideo
+	mov	gs, ax			; 视频段选择子(目的)
+
+	mov	edi, (80 * 12 + 0) * 2	; 屏幕第 12 行, 第 0 列。
+	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+	mov	al, 'C'
+	mov	[gs:edi], ax
+
+	retf
+
+SegCodeDestLen	equ	$ - LABEL_SEG_CODE_DEST
+; END of [SECTION .sdest]
 
 
 ; 16 位代码段. 由 32 位代码段跳入, 跳出后到实模式
@@ -288,7 +327,7 @@ LABEL_CODE_A:
 	mov	ax, SelectorVideo
 	mov	gs, ax			; 视频段选择子(目的)
 
-	mov	edi, (80 * 12 + 0) * 2	; 屏幕第 10 行, 第 0 列。
+	mov	edi, (80 * 13 + 0) * 2	; 屏幕第 13 行, 第 0 列。
 	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
 	mov	al, 'L'
 	mov	[gs:edi], ax
